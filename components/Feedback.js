0b6316a907d4c28807426a093c1cbc78af51a555
Mauricio Paternina <spaceinvadev@gmail.com>
2021-03-19 10:42:29 -0500

    [feat]: set up form to save feedback data

--- components/Feedback.js
+++ components/Feedback.js
@@ -2,14 +2,16 @@ import { Box, Heading, Text, Divider } from '@chakra-ui/react';
 import { format, parseISO } from 'date-fns';
 
 const Feedback = ({ author, text, createdAt }) => (
-  <Box borderRadius={4} maxWidth='700px' w='full'>
+  <Box borderRadius={4} maxWidth='700px' w='full' mb={8} paddingInlineStart={2}>
     <Heading size='sm' as='h3' mb={0} color='gray.900' fontWeight='medium'>
       {author}
     </Heading>
-    <Text color='gray.500' mb={4} fontSize=''>
+    <Text color='gray.500' mb={4} fontSize='xs'>
       {format(parseISO(createdAt), 'PPpp')}
     </Text>
-    <Text color='gray.800'>{text}</Text>
+    <Text color='gray.800' mb={2}>
+      {text}
+    </Text>
     <Divider borderColor='gray.200' backgroundColor='gray.200' />
   </Box>
 );
--- pages/p/[siteId].js
+++ pages/p/[siteId].js
@@ -1,10 +1,15 @@
-import { getAllFeedback, getAllSites } from '@/lib/db-admin';
-import Feedback from '@/components/Feedback';
+import { useState } from 'react';
 import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
+import Feedback from '@/components/Feedback';
+import { useAuth } from '@/lib/auth';
+import { getAllFeedback, getAllSites } from '@/lib/db-admin';
+import { createFeedback } from '@/lib/db';
 
 export async function getStaticProps(context) {
   const siteId = context.params.siteId;
-  const feedback = await getAllFeedback(siteId);
+  const { feedback } = await getAllFeedback(siteId);
 
   return {
     props: {
@@ -14,7 +19,7 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
-  const sites = await getAllSites();
+  const { sites } = await getAllSites();
   const paths = sites.map((site) => ({
     params: {
       siteId: site.id.toString(),
@@ -28,6 +33,35 @@ export async function getStaticPaths() {
 }
 
 const SiteFeedback = ({ initialFeedback }) => {
+  const auth = useAuth();
+  const router = useRouter();
+
+  // Local mutation for displaying new feedback immediately
+  const [allFeedback, setAllFeedback] = useState(initialFeedback);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // Get comment
+    const newFeedback = {
+      author: auth.user.name,
+      authorId: auth.user.uid,
+      siteId: router.query.siteId,
+      // text could've been fetched via useRef
+      text: e.target.elements.comment.value,
+      createdAt: new Date().toISOString(),
+      provider: auth.user.provider,
+      status: 'pending',
+    };
+
+    // Save to db
+    setAllFeedback([newFeedback, ...allFeedback]);
+    createFeedback(newFeedback);
+
+    // Clear input
+    e.target.elements.comment.value = '';
+  };
+
   return (
     <Box
       display='flex'
@@ -36,14 +70,16 @@ const SiteFeedback = ({ initialFeedback }) => {
       maxWidth='700px'
       margin='0 auto'
     >
-      <FormControl id='comment' my={8}>
-        <FormLabel htmlFor='comment'>Comment</FormLabel>
-        <Input type='comment' id='comment' mb={4} />
-        <Button type='submit' fontWeight='medium'>
-          Add Comment
-        </Button>
-      </FormControl>
-      {initialFeedback.map((feedback) => (
+      <Box as='form' onSubmit={onSubmit}>
+        <FormControl id='comment' my={8}>
+          <FormLabel htmlFor='comment'>Comment</FormLabel>
+          <Input type='comment' id='comment' mb={4} />
+          <Button type='submit' fontWeight='medium'>
+            Add Comment
+          </Button>
+        </FormControl>
+      </Box>
+      {allFeedback.map((feedback) => (
         <Feedback key={feedback.id} {...feedback} />
       ))}
     </Box>
